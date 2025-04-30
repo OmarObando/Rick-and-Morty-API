9bbdc76e7233be897564c847081562076e7980ba
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2025-04-30 16:25:10 -0600

    [BUILD] Initial Structure API

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3200;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+});
