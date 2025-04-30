75f27a2947c22ea7d18a7125f2fedc3bee6f0c0a
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2025-04-30 17:15:29 -0600

    [FEAT:fix] Updated comment

--- src/services/character-service.js
+++ src/services/character-service.js
@@ -1,8 +1,7 @@
 const axios  = require('axios');
-
+//We could use differents folders for each service, but in this case we aren't going to do it 
 //RAM = Rick and Morty API
 const RAM_URL = process.env.RAM_URL || 'https://rickandmortyapi.com/api/character';
-
 
 const getCharacters = async (page = 1) => {
     let url = RAM_URL;
