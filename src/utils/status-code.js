8970ad14fd84ac267a2915cec224470baa9bf978
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2025-04-30 17:34:21 -0600

    [HOTFIX] codes

--- src/utils/status-code.js
+++ src/utils/status-code.js
@@ -1,6 +1,7 @@
 const codes = {
     OK : 200,
     CREATED : 201,
+    FORBIDDEN : 403,
     UNAUTORIZED : 401,
     SERVER_ERROR : 500
 }
--- src/validators/handleValidator.js
+++ src/validators/handleValidator.js
@@ -1,11 +1,12 @@
 const {validationResult} = require('express-validator');
+const {codes} = require('../utils/status-code');
 
 const validateResults = (req, res, next) => {
     try{
         validationResult(req).throw();
         return next()
     }catch(err){
-        res.status(403);
+        res.status(codes.FORBIDDEN);
         res.send({errors: err.array()});
     }
 };
