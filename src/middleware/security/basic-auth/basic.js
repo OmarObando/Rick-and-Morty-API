b0290b6aa25968c75f7812f49fa3a248188c7960
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2025-04-30 16:44:46 -0600

    [ADD] Basic Auth module added

---
+++ src/middleware/security/basic-auth/basic.js
@@ -0,0 +1,27 @@
+const basicAuth = require('basic-auth');
+const {codes} = require('../../../utils/status-code');
+const {statusMessages} = require('../../../utils/status-messages');
+const {credentials} = require('./credentials');
+
+const verifyBasicAuth = (req, res, next) => {
+    const auth = basicAuth(req);
+    if (!auth || !auth.name || !auth.pass) {
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        res.status(codes.UNAUTORIZED).send(statusMessages.UNAUTORIZED);
+        return;
+    }
+    if(!checkCredentials(auth.name, auth.pass)) {
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        res.status(codes.UNAUTORIZED).send(statusMessages.UNAUTORIZED);
+        return;
+    }
+};
+
+const checkCredentials = (user, pass) => {
+    if (user === credentials.user && pass === credentials.password) {
+        return true;
+    }
+    return false;
+};
+
+module.exports = {verifyBasicAuth};
---
+++ src/utils/status-code.js
@@ -0,0 +1,8 @@
+const codes = {
+    OK : 200,
+    CREATED : 201,
+    UNAUTORIZED : 401,
+    SERVER_ERROR : 500
+}
+
+module.exports = {codes};
