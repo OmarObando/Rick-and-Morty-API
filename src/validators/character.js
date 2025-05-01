42f19935cfbf11334533495c247b0ae31b03a03d
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2025-04-30 18:06:41 -0600

    [FEAT: fix] Fixed validator

--- src/validators/character.js
+++ src/validators/character.js
@@ -2,7 +2,7 @@ const {query} = require('express-validator')
 const validateResults = require('./handleValidator')   
 
 const validateGetCharacters = [
-    query('page').optional().isInt({min: 1, max: 43}).withMessage('Page must be an integer between 1 and 43'),
+    query('page').optional().isInt({min: 1, max: 42}).withMessage('Page must be an integer between 1 and 42'),
     (req, res, next) => validateResults(req, res, next)
 ]
 
